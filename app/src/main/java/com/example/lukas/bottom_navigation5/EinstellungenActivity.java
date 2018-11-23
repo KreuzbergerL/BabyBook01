2e451584902393e7079053a18963f1127addb378
KreuzbergerL <l.kreuzberger@gmx.at>
2018-11-23 10:12:55 +0100

    "Einstellungen" Activity mit Eingabe vom geburtstermin hinzugefügt

--- app/src/main/java/com/example/lukas/bottom_navigation5/EinstellungenActivity.java
+++ app/src/main/java/com/example/lukas/bottom_navigation5/EinstellungenActivity.java
@@ -1,6 +1,8 @@
 package com.example.lukas.bottom_navigation5;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import com.example.lukas.bottom_navigation5.MainActivity;
 
 import java.util.Calendar;
 
@@ -17,9 +20,12 @@ public class EinstellungenActivity extends AppCompatActivity {
 
     private static final String TAG = "EinstellungenActivity";
 
-    private TextView mDisplayDate;  // vorher ist das Datum ja bei der gleichen Textview angezeigt worden, wo man auch draufgeklickt hat
-    private TextView DatumsAnzeige; //Jetzt wird das Datum an einer zweiten Textview angezeigt
+    public TextView mDisplayDate;  // vorher ist das Datum ja bei der gleichen Textview angezeigt worden, wo man auch draufgeklickt hat
+    public TextView DatumsAnzeige; //Jetzt wird das Datum an einer zweiten Textview angezeigt
     private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,7 @@ public class EinstellungenActivity extends AppCompatActivity {
 
         DatumsAnzeige = (TextView) findViewById(R.id.date_display);
         mDisplayDate = (TextView) findViewById(R.id.tvDate);
+
 
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,9 +62,28 @@ public class EinstellungenActivity extends AppCompatActivity {
 
                 String date = month + "/" + day + "/" + year;
                DatumsAnzeige.setText(date);
+                saveInfo();
             }
+
+
         };
+
+
     }
+
+
+    public void saveInfo()
+    {
+        SharedPreferences file1 = getSharedPreferences("date_info", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = file1.edit();
+
+        editor.putString("Datum", DatumsAnzeige.getText().toString());
+        editor.apply();
+        editor.commit();
+    }
+
+
+
 }
 
 
--- app/src/main/java/com/example/lukas/bottom_navigation5/MainActivity.java
+++ app/src/main/java/com/example/lukas/bottom_navigation5/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lukas.bottom_navigation5;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,21 +12,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.example.lukas.bottom_navigation5.*;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    public TextView vDisplay;
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        vDisplay = (TextView) findViewById(R.id.textView2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar01);
+        button =(Button) findViewById(R.id.button);
         setSupportActionBar(toolbar);
 
         BottomNavigationView bottomNav = findViewById(R.id.navigation);     //Von mir hinzugefügt
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
     }
 //---------------------Aufmachen des Einstellungen Buttons beim anklicken
     @Override
@@ -67,5 +79,18 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+//-------------------------------------Button Onclick----------------------------------------------
+
+
+
+    public void displaydata()
+    {
+        SharedPreferences file1 = getSharedPreferences("date_info", Context.MODE_PRIVATE);
+        String Datum = file1.getString("Datum", "");
+        vDisplay.setText(Datum);
+    }
+
+
+
 
 }
